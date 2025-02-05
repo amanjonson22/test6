74352963379c649ae8ffabbba554bd4f13a1cd09
amanda <amandacsr10@gmail.com>
2025-02-05 17:26:56 -0300

    Initial Commit

---
+++ test6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test6
+//
+//  Created by AMANDA CAROLINE DA SILVA RODRIGUES on 05/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test6/test6App.swift
@@ -0,0 +1,17 @@
+//
+//  test6App.swift
+//  test6
+//
+//  Created by AMANDA CAROLINE DA SILVA RODRIGUES on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct test6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
